e5d9d91165cb06321b9adaf4c808a78b7983c581
Miguel de Icaza <miguel@gnome.org>
2015-06-04 15:03:32 -0400

    Add binder

---
+++ SharpieBinder/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Clang.Ast;
+using System.IO;
+
+namespace SharpieBinder
+{
+	
+	class MainClass
+	{
+		const string output = "/cvs/urho/bindings/generated";
+
+		public static int Main (string[] args)
+		{
+			Directory.CreateDirectory (output);
+			Console.WriteLine("This needs a 64-bit Mono to run, and libclang-mono.dylib in /usr/lib");
+			if (args.Length == 0) {
+				//Console.Error.WriteLine ("error: provide a PCH file to dump");
+				//return 1;
+				args = new String[] { "/Users/miguel/Dropbox/UrhoBindings/test.pch" };
+			}
+
+			var reader = new AstReader ();
+			var binder = new CxxBinder();
+			reader.TranslationUnitParsed += tu => tu.Accept (binder);
+			reader.Load (args [0]);
+			foreach (var st in binder.Generate()) {
+				File.WriteAllText (output + "/" + st.FileName, st.ToString ());
+				//File.WriteAllText (output + "/" + st.FileName + ".c"
+			}
+			Console.WriteLine("Dumped data into /tmp/ra");
+			
+			return 0;
+		}
+	}
+}
---
+++ SharpieBinder/README.md
@@ -0,0 +1,25 @@
+﻿Requirements
+============
+
+* ObjectiveSharpie built and installed
+* 64-bit Mono
+* libclang-mono.dylib installed in /usr/lib (from ObjectiveSharpie)
+* PCH file generated from parsing all Urho3D header files
+
+When executed, currently dumps the C# bindigns in /tmp/ra
+
+Design Binding Notes
+====================
+
+The API in Urho3D is luckily not designed to be used as an object oriented toolbox,
+so it is rare the case where users must subclass.   This has the advantage for a
+binding that there is no need to support the scenario where subclasses in C# must
+be able to override methods in the system.
+
+Consideration: should we allow subclassing of the Urho classes?  If we do, then
+we need to have a "Runtime.GetObejct"-like system to ensure that given an unmanaged
+pointer, we always return the same instance to managed code.
+
+If not, we could just make all wrappers be dumb wrappers and override the Equals,
+operator == and operator != methods.
+
