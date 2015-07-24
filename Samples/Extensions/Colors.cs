2a88a077b2ec5bad46415c3a09c01ae0eb62b7c1
Egor Bogatov <egorbo@gmail.com>
2015-07-24 17:36:24 +0300

    predefined Colors collection (temporarily)

---
+++ Samples/Extensions/Colors.cs
@@ -0,0 +1,15 @@
+﻿using Urho;
+
+public static class Colors
+{
+    public static readonly Color White = new Color();
+    public static readonly Color Gray = new Color(0.5f, 0.5f, 0.5f);
+    public static readonly Color Black = new Color(0.0f, 0.0f, 0.0f);
+    public static readonly Color Red = new Color(1.0f, 0.0f, 0.0f);
+    public static readonly Color Green = new Color(0.0f, 1.0f, 0.0f);
+    public static readonly Color Blue = new Color(0.0f, 0.0f, 1.0f);
+    public static readonly Color Cyan = new Color(0.0f, 1.0f, 1.0f);
+    public static readonly Color Magenta = new Color(1.0f, 0.0f, 1.0f);
+    public static readonly Color Yellow = new Color(1.0f, 1.0f, 0.0f);
+    public static readonly Color Transparent = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+}
