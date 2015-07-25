1bd74c9cd3803db0be712a462559cdefa341e853
Egor Bogatov <egorbo@gmail.com>
2015-07-25 21:05:24 +0300

    02_HelloGUI sample

--- Samples/Samples/Todo/02_HelloGUI.cs
+++ Samples/Samples/Todo/02_HelloGUI.cs
@@ -2,56 +2,184 @@
 
 class _02_HelloGUI : Sample
 {
-    private Scene scene;
-    private bool drawDebug;
-    private Camera camera;
+    private Window window;
+    private UIElement uiRoot;
+    private IntVector2 dragBeginPosition;
 
-    public _02_HelloGUI(Context ctx) : base(ctx) { }
+    public _02_HelloGUI(Context ctx) : base(ctx)
+    {
+        uiRoot = UI.Root;
+        dragBeginPosition = new IntVector2(0, 0);
+    }
 
     public override void Start()
     {
         base.Start();
-        CreateScene();
-        SimpleCreateInstructionsWithWASD("\nSpace to toggle debug geometry");
-        SetupViewport();
-        SubscribeToEvents();
+        Input.SetMouseVisible(true, false);
+        // Load XML file containing default UI style sheet
+        var cache = ResourceCache;
+        XMLFile style = cache.GetXmlFile("UI/DefaultStyle.xml");
+
+        // Set the loaded style as default style
+        uiRoot.SetDefaultStyle(style);
+
+        // Initialize Window
+        InitWindow();
+
+        // Create and add some controls to the Window
+        InitControls();
+
+        // Create a draggable Fish
+        CreateDraggableFish();
     }
 
-    private void SubscribeToEvents()
+    private void InitControls()
     {
-        SubscribeToUpdate(args =>
-        {
-            SimpleMoveCamera(args.TimeStep);
-            if (Input.GetKeyDown(' '))
-                drawDebug = !drawDebug;
-        });
+        // Create a CheckBox
+        CheckBox checkBox = new CheckBox(Context);
+        checkBox.Name="CheckBox";
 
-        SubscribeToPostRenderUpdate(args =>
-        {
-            // If draw debug mode is enabled, draw viewport debug geometry, which will show eg. drawable bounding boxes and skeleton
-            // bones. Note that debug geometry has to be separately requested each frame. Disable depth test so that we can see the
-            // bones properly
-            if (drawDebug)
-                Renderer.DrawDebugGeometry(false);
-        });
+        // Create a Button
+        Button button = new Button(Context);
+        button.Name="Button";
+        button.MinHeight=24;
+
+        // Create a LineEdit
+        LineEdit lineEdit = new LineEdit(Context);
+        lineEdit.Name="LineEdit";
+        lineEdit.MinHeight=24;
+
+        // Add controls to Window
+        window.AddChild(checkBox);
+        window.AddChild(button);
+        window.AddChild(lineEdit);
+
+        // Apply previously set default style
+        checkBox.SetStyleAuto(null);
+        button.SetStyleAuto(null);
+        lineEdit.SetStyleAuto(null);
     }
-    
-    private void SetupViewport()
+
+    private void InitWindow()
     {
-        var renderer = Renderer;
-        renderer.SetViewport(0, new Viewport(Context, scene, CameraNode.GetComponent<Camera>(), null));
+        // Create the Window and add it to the UI's root node
+        window = new Window(Context);
+        uiRoot.AddChild(window);
+
+        // Set Window size and layout settings
+        window.SetMinSize(384, 192);
+#warning MISSING_API UIElement::SetLayout
+        ////window_.SetLayout(LM_VERTICAL, 6, new IntRect(6, 6, 6, 6));
+        window.SetAlignment(HorizontalAlignment.HA_CENTER, VerticalAlignment.VA_CENTER);
+        window.Name="Window";
+
+        // Create Window 'titlebar' container
+        UIElement titleBar = new UIElement(Context);
+        titleBar.SetMinSize(0, 24);
+        titleBar.VerticalAlignment = VerticalAlignment.VA_TOP;
+        titleBar.LayoutMode= LayoutMode.LM_HORIZONTAL;
+
+        // Create the Window title Text
+        var windowTitle = new Text(Context);
+        windowTitle.Name="WindowTitle";
+#warning MISSING_API Text::Text
+        //windowTitle.SetText("Hello GUI!");
+
+        // Create the Window's close button
+        Button buttonClose = new Button(Context);
+        buttonClose.Name="CloseButton";
+
+        // Add the controls to the title bar
+        titleBar.AddChild(windowTitle);
+        titleBar.AddChild(buttonClose);
+
+        // Add the title bar to the Window
+        window.AddChild(titleBar);
+
+        // Apply styles
+        window.SetStyleAuto(null);
+        windowTitle.SetStyleAuto(null);
+        buttonClose.SetStyle("CloseButton", null);
+
+        SubscribeToReleased(HandleClosePressed);
+
+        // Subscribe also to all UI mouse clicks just to see where we have clicked
+        SubscribeToUIMouseClick(HandleControlClicked);
     }
 
-    private void CreateScene()
+    private void CreateDraggableFish()
     {
         var cache = ResourceCache;
-        scene = new Scene(Context);
+        var graphics = Graphics;
 
-        // Create the camera. Limit far clip distance to match the fog
-        CameraNode = scene.CreateChild("Camera");
-        camera = CameraNode.CreateComponent<Camera>();
-        camera.FarClip = 300.0f;
-        // Set an initial position for the camera scene node above the plane
-        CameraNode.Position = new Vector3(0.0f, 5.0f, 0.0f);
+        // Create a draggable Fish button
+        Button draggableFish = new Button(Context);
+        draggableFish.Texture=cache.GetTexture2D("Textures/UrhoDecal.dds"); // Set texture
+        draggableFish.BlendMode= BlendMode.BLEND_ADD;
+        draggableFish.SetSize(128, 128);
+        draggableFish.SetPosition((graphics.Width - draggableFish.Width) / 2, 200);
+        draggableFish.Name="Fish";
+        uiRoot.AddChild(draggableFish);
+
+        // Add a tooltip to Fish button
+        ToolTip toolTip = new ToolTip(Context);
+        draggableFish.AddChild(toolTip);
+        toolTip.Position = new IntVector2(draggableFish.Width + 5, draggableFish.Width / 2); // slightly offset from close button
+        BorderImage textHolder = new BorderImage(Context);
+        toolTip.AddChild(textHolder);
+        textHolder.SetStyle("ToolTipBorderImage", null);
+        var toolTipText = new Text(Context);
+        textHolder.AddChild(toolTipText);
+        toolTipText.SetStyle("ToolTipText", null);
+#warning MISSING_API Text::Text
+        //toolTipText.SetText("Please drag me!");
+
+        // Subscribe draggableFish to Drag Events (in order to make it draggable)
+        // See "Event list" in documentation's Main Page for reference on available Events and their eventData
+        SubscribeToDragBegin(HandleDragBegin);
+        SubscribeToDragMove(HandleDragMove);
+        SubscribeToDragEnd(HandleDragEnd);
+    }
+
+    private void HandleDragBegin(DragBeginEventArgs args)
+    {
+        // Get UIElement relative position where input (touch or click) occured (top-left = IntVector2(0,0))
+        dragBeginPosition = new IntVector2(args.ElementX, args.ElementY);
+    }
+
+    private void HandleDragMove(DragMoveEventArgs args)
+    {
+        IntVector2 dragCurrentPosition = new IntVector2(args.X, args.Y);
+#warning MISSING_API IntVector2 overloaded operators +, -
+        //args.Element.Position =  (dragCurrentPosition - dragBeginPosition_);
+    }
+
+    private void HandleDragEnd(DragEndEventArgs args) // For reference (not used here)
+    {
+    }
+
+    private void HandleClosePressed(ReleasedEventArgs args)
+    {
+        Engine.Exit();
+    }
+
+    private void HandleControlClicked(UIMouseClickEventArgs args)
+    {
+        // Get the Text control acting as the Window's title
+        var windowTitle = window.GetChild("WindowTitle", true) as Text;
+
+        // Get control that was clicked
+        UIElement clicked = args.Element;
+
+        string name = "...?";
+        if (clicked != null)
+        {
+            // Get the name of the control that was clicked
+            name = clicked.Name;
+        }
+
+        // Update the Window's title text
+#warning MISSING_API Text::Text
+        //windowTitle.SetText("Hello " + name + "!");
     }
 }
