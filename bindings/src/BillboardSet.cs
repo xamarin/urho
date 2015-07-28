fe6e046dcdba3bcf17b5e3759f828c52731bc663
Egor Bogatov <egorbo@gmail.com>
2015-07-28 16:20:07 +0300

    fix compilation issue

--- bindings/src/BillboardSet.cs
+++ bindings/src/BillboardSet.cs
@@ -5,7 +5,7 @@ namespace Urho {
 		public Billboard? GetBillboardSafe (uint index)
 		{
 			unsafe {
-				var *result = BillboardSet_GetBillboard (handle, index);
+                Billboard* result = BillboardSet_GetBillboard (handle, index);
 				if (result == null)
 					return null;
 				return *result;
