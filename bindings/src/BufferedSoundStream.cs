305fabf6db8262db559d0b4bfc89bd80293e11ba
Miguel de Icaza <miguel@gnome.org>
2015-07-30 21:53:40 -0400

    Add BufferedSoundStream.AddData overloads

---
+++ bindings/src/BufferedSoundStream.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Urho {
+	public partial class BufferedSoundStream {
+		public void AddData (byte [] data, int start = 0, int count = -1)
+		{
+			if (data == null)
+				throw new ArgumentNullException ("data");
+			if (start < 0)
+				throw new ArgumentException ("start should be positive");
+			if (count > 0 && start + count > data.Length)
+				throw new ArgumentException ("start and count would go beyond the array");
+
+			unsafe {
+				fixed (byte *p = &data[start])
+					BufferedSoundStream_AddData (handle, (IntPtr) p, (uint)(count == -1 ? data.Length-start : count));
+			}
+		}
+
+		public void AddData (short [] data, int start = 0, int count = -1)
+		{
+			if (data == null)
+				throw new ArgumentNullException ("data");
+			if (start < 0)
+				throw new ArgumentException ("start should be positive");
+			if (count > 0 && start + count > data.Length)
+				throw new ArgumentException ("start and count would go beyond the array");
+
+			unsafe {
+				fixed (short *p = &data[start])
+					BufferedSoundStream_AddData (handle, (IntPtr) p, (uint)(2 * (count == -1 ? data.Length-start : count)));
+			}
+		}
+	}
+}
