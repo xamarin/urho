e76f1bf3e69eb01597d317ca57b9ea45ed569ed1
EgorBo <egorbo@gmail.com>
2015-08-26 17:32:17 +0300

    [Bindings] add global console methods

---
+++ bindings/src/UrhoConsole.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Urho
+{
+	partial class UrhoConsole
+	{
+		[DllImport("mono-urho", CallingConvention = CallingConvention.Cdecl)]
+		internal static extern void Console_OpenConsoleWindow();
+		
+		[DllImport("mono-urho", CallingConvention = CallingConvention.Cdecl)]
+		internal static extern IntPtr Console_GetConsoleInput();
+
+		public static void OpenConsoleWindow()
+		{
+			Console_OpenConsoleWindow();
+		}
+
+		public static string GetConsoleInput()
+		{
+			return Marshal.PtrToStringAnsi(Console_GetConsoleInput());
+		}
+	}
+}
