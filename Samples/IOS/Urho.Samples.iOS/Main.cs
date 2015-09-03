0dae823b0ad6a91f8316234cc9840e7df6ed05fa
EgorBo <egorbo@gmail.com>
2015-09-03 03:58:06 +0300

    [IOS] blank project + references

---
+++ Samples/IOS/Urho.Samples.iOS/Main.cs
@@ -0,0 +1,14 @@
+﻿using UIKit;
+
+namespace Urho.Samples.iOS
+{
+	public class Application
+	{
+		// This is the main entry point of the application.
+		static void Main(string[] args)
+		{
+			Urho.Application.RegisterSdlLauncher(() => new _23_Water(new Context()));
+			UIApplication.Main(args, null, "AppDelegate");
+		}
+	}
+}
