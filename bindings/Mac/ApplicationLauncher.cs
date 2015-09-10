6ece2d457e3575c630cd05d9239b67749fb79b9f
EgorBo <egorbo@gmail.com>
2015-09-11 00:16:32 +0300

    Add Urho.Mac and Urho.Windows assemblies. They contain native libs as embedded resource (and extract them on first launch).

---
+++ bindings/Mac/ApplicationLauncher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace Urho
+{
+	public static class ApplicationLauncher
+	{
+		public static void Run(Application application)
+		{
+			const string libName = "libmono-urho.dylib";
+            if (System.IO.File.Exists(libName))
+				return;
+
+			var asm = Assembly.GetExecutingAssembly();
+			using (var erStream = asm.GetManifestResourceStream($"{asm.FullName}.{libName}"))
+			using (var fileStream = System.IO.File.Create(libName))
+			{
+				if (erStream == null)
+					throw new InvalidOperationException($"{libName} was not found as embedded resource");
+
+				erStream.CopyTo(fileStream);
+			}
+		}
+	}
+}
