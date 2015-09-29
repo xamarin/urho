ce8253e11b89b1fbddda8bc09fd078b678d9e7b4
EgorBo <egorbo@gmail.com>
2015-09-29 23:10:29 +0300

    Fix SharpieBinder paths

--- SharpieBinder/Program.cs
+++ SharpieBinder/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Clang.Ast;
 using System.IO;
 
@@ -8,27 +7,24 @@ namespace SharpieBinder
 	
 	class MainClass
 	{
-		const string output = "/cvs/urho/bindings/generated";
+		const string Output = "../../bindings/generated";
 
 		public static int Main (string[] args)
 		{
-			Directory.CreateDirectory (output);
+			Directory.CreateDirectory (Output);
 			Console.WriteLine(Environment.CurrentDirectory);
 
 			if (System.Runtime.InteropServices.Marshal.SizeOf(typeof (IntPtr)) == 4) {
-				Console.Error.WriteLine ("This needs a 64-bit Mono to run, and libclang-mono.dylib in /usr/lib");
+				Console.Error.WriteLine ("This needs a 64-bit Mono to run");
 				return 1;
 			}
 
 			if (args.Length == 0) {
-				//Console.Error.WriteLine ("error: provide a PCH file to dump");
-				//return 1;
-				args = new String[] { "/cvs/urho/bindings/Urho.pch" };
-				//args = new String[] { "/Users/miguel/Dropbox/UrhoBindings/test.pch" };
+				args = new [] { "../../bindings/Urho.pch" };
 			}
 
 			var reader = new AstReader ();
-			var binder = new CxxBinder(output);
+			var binder = new CxxBinder(Output);
 			var lookup = new ScanBaseTypes ();
 
 			reader.TranslationUnitParsed += tu => { 
@@ -42,12 +38,12 @@ namespace SharpieBinder
 			binder.FixupOverrides();
 
 			foreach (var st in binder.Generate()) {
-				File.WriteAllText (output + "/" + st.FileName, st.ToString ());
-				//File.WriteAllText (output + "/" + st.FileName + ".c"
+				File.WriteAllText (Output + "/" + st.FileName, st.ToString ());
 			}
+
 			foreach (var a in binder.unhandledEnums)
 				Console.WriteLine ("Missing special Enum processing for {0}", a);
-			Console.WriteLine($"Dumped data into {output}");
+			Console.WriteLine($"Dumped data into {Output}");
 			binder.Close ();
 			return 0;
 		}
