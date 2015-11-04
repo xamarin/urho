14abf2193ed5f509b7a524994c4ed3ba2c3fdb12
EgorBo <egorbo@gmail.com>
2015-11-04 19:48:55 +0300

    iOS: add UrhoSurface

---
+++ bindings/iOS/UrhoSurface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UIKit;
+
+namespace Urho.iOS
+{
+	public class UrhoSurface : UIView
+	{
+		[DllImport("mono-urho", CallingConvention = CallingConvention.Cdecl)]
+		static extern void SDL_SetExternalViewPlaceholder(IntPtr viewPtr, IntPtr windowPtr);
+
+		public override void WillMoveToWindow(UIWindow window)
+		{
+			SDL_SetExternalViewPlaceholder(this.Handle, window.Handle);
+			base.WillMoveToWindow(window);
+		}
+	}
+}
