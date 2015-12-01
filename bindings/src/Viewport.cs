5a9630f7af8892449ac39549fcafde7c54f1c451
EgorBo <egorbo@gmail.com>
2015-12-01 17:02:34 +0300

    Add Context-less ctors to Viewport

---
+++ bindings/src/Viewport.cs
@@ -0,0 +1,11 @@
+﻿namespace Urho
+{
+	partial class Viewport
+	{
+		public Viewport(Scene scene, Camera camera, RenderPath renderPath) 
+			: this(Application.CurrentContext, scene, camera, renderPath) {}
+
+		public Viewport(Scene scene, Camera camera, Urho.IntRect rect, RenderPath renderPath) 
+			: this(Application.CurrentContext, scene, camera, rect, renderPath) {}
+	}
+}
