fd6907065d8e584438db1541a785333e10e51abe
EgorBo <egorbo@gmail.com>
2015-12-15 15:41:18 -0500

    fix RemoveNugetFromSamples script

--- bindings/RemoveNugetFromSamples.cs
+++ bindings/RemoveNugetFromSamples.cs
@@ -27,38 +27,38 @@ class P
 		//FeatureSamples.Core
 		var coreCsproj = @"Samples/FeatureSamples/Core/Urho.Samples.csproj";
 		var coreDoc = XDocument.Load(coreCsproj);
-		ReplaceRef(coreDoc, "Urho, ", "{641886db-2c6c-4d33-88da-97bec0ec5f86}", @"..\..\..\bindings\Urho.csproj", "Urho");
+		ReplaceRef(coreDoc, "Urho", "{641886db-2c6c-4d33-88da-97bec0ec5f86}", @"..\..\..\bindings\Urho.csproj", "Urho");
 		coreDoc.Save(coreCsproj);
 
 		//FeatureSamples.Droid
 		var droidCsproj = @"Samples/FeatureSamples/Android/Urho.Samples.Droid.csproj";
 		var droidDoc = XDocument.Load(droidCsproj);
-		ReplaceRef(droidDoc, "Urho, ", "{641886db-2c6c-4d33-88da-97bec0ec5f86}", @"..\..\..\bindings\Urho.csproj", "Urho");
-		ReplaceRef(droidDoc, "Urho.Droid, ", "{f0c1189b-75f7-4bd8-b394-a23d5b03eff6}", @"..\..\..\Bindings\Android\Urho.Droid.csproj", "Urho.Droid");
-		ReplaceRef(droidDoc, "Urho.Droid.SdlBindings, ", "{9438f1bb-e800-48c0-95ce-f158a60abd36}", @"..\..\..\Bindings\Android\Urho.Android.SdlBindings\Urho.Droid.SdlBindings.csproj", "Urho.Droid.SdlBindings");
+		ReplaceRef(droidDoc, "Urho", "{641886db-2c6c-4d33-88da-97bec0ec5f86}", @"..\..\..\bindings\Urho.csproj", "Urho");
+		ReplaceRef(droidDoc, "Urho.Droid", "{f0c1189b-75f7-4bd8-b394-a23d5b03eff6}", @"..\..\..\Bindings\Android\Urho.Droid.csproj", "Urho.Droid");
+		ReplaceRef(droidDoc, "Urho.Droid.SdlBindings", "{9438f1bb-e800-48c0-95ce-f158a60abd36}", @"..\..\..\Bindings\Android\Urho.Android.SdlBindings\Urho.Droid.SdlBindings.csproj", "Urho.Droid.SdlBindings");
 		RemoveTargets(droidDoc);
 		droidDoc.Save(droidCsproj);
 
 		//FeatureSamples.iOS
 		var iosCsproj = @"Samples/FeatureSamples/iOS/Urho.Samples.iOS.csproj";
 		var iosDoc = XDocument.Load(iosCsproj);
-		ReplaceRef(iosDoc, "Urho, ", "{641886db-2c6c-4d33-88da-97bec0ec5f86}", @"..\..\..\bindings\Urho.csproj", "Urho");
-		ReplaceRef(iosDoc, "Urho.iOS, ", "{9ae80bd9-e1e2-41da-bb6f-712b35028bd9}", @"..\..\..\Bindings\iOS\Urho.iOS.csproj", "Urho.iOS");
+		ReplaceRef(iosDoc, "Urho", "{641886db-2c6c-4d33-88da-97bec0ec5f86}", @"..\..\..\bindings\Urho.csproj", "Urho");
+		ReplaceRef(iosDoc, "Urho.iOS", "{9ae80bd9-e1e2-41da-bb6f-712b35028bd9}", @"..\..\..\Bindings\iOS\Urho.iOS.csproj", "Urho.iOS");
 		RemoveTargets(iosDoc);
 		iosDoc.Save(iosCsproj);
 
 		//FeatureSamples.Desktop
 		var desktopCsproj = @"Samples/FeatureSamples/Desktop/Urho.Samples.Desktop.csproj";
 		var desktopDoc = XDocument.Load(desktopCsproj);
-		ReplaceRef(desktopDoc, "Urho, ", "{641886db-2c6c-4d33-88da-97bec0ec5f86}", @"..\..\..\bindings\Urho.csproj", "Urho");
-		ReplaceRef(desktopDoc, "Urho.Desktop, ", "{F0359D5E-D6D4-47D3-A9F0-5A97C31DC476}", @"..\..\..\Bindings\Desktop\Urho.Desktop.csproj", "Urho.Desktop");
+		ReplaceRef(desktopDoc, "Urho", "{641886db-2c6c-4d33-88da-97bec0ec5f86}", @"..\..\..\bindings\Urho.csproj", "Urho");
+		ReplaceRef(desktopDoc, "Urho.Desktop", "{F0359D5E-D6D4-47D3-A9F0-5A97C31DC476}", @"..\..\..\Bindings\Desktop\Urho.Desktop.csproj", "Urho.Desktop");
 		RemoveTargets(desktopDoc);
 		desktopDoc.Save(desktopCsproj);
 	}
 
 	static void ReplaceRef(XDocument doc, string refName, string id, string path, string name)
 	{
-		var node = doc.XPathSelectElement($"//p:Reference[starts-with(@Include, '{refName}')]", nsManager);
+		var node = doc.XPathSelectElement($"//p:Reference[@Include='{refName}']", nsManager);
 		if (node == null)//means it's already converted to ProjectReference
 			return;
 		node.Parent.Add(new XElement(ns + "ProjectReference", new XAttribute("Include", path),
