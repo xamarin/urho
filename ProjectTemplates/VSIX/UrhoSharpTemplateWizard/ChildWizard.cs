acde13d500f3e55a8f1642e193f2c61714d0fd9f
EgorBo <egorbo@gmail.com>
2016-01-29 13:32:39 +0300

    Add UrhoSharpTemplateWizard to VSIX

---
+++ ProjectTemplates/VSIX/UrhoSharpTemplateWizard/ChildWizard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace UrhoSharpTemplateWizard
+{
+	public class ChildWizard : IWizard
+	{
+		// Retrieve global replacement parameters
+		public void RunStarted(object automationObject,
+			Dictionary<string, string> replacementsDictionary,
+			WizardRunKind runKind, object[] customParams)
+		{
+			// Add custom parameters.
+			replacementsDictionary.Add("$saferootprojectname$", RootWizard.GlobalDictionary["$saferootprojectname$"]);
+		}
+
+		public void RunFinished()
+		{
+		}
+
+		public void BeforeOpeningFile(ProjectItem projectItem)
+		{
+		}
+
+		public void ProjectFinishedGenerating(Project project)
+		{
+		}
+
+		public bool ShouldAddProjectItem(string filePath)
+		{
+			return true;
+		}
+
+		public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+		{
+		}
+	}
+}
---
+++ ProjectTemplates/VSIX/UrhoSharpTemplateWizard/RootWizard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace UrhoSharpTemplateWizard
+{
+	public class RootWizard : IWizard
+	{
+		// Use to communicate $saferootprojectname$ to ChildWizard
+		public static Dictionary<string, string> GlobalDictionary =
+			new Dictionary<string, string>();
+
+		// Add global replacement parameters
+		public void RunStarted(object automationObject,
+			Dictionary<string, string> replacementsDictionary,
+			WizardRunKind runKind, object[] customParams)
+		{
+			// Place "$saferootprojectname$ in the global dictionary.
+			// Copy from $safeprojectname$ passed in my root vstemplate
+			GlobalDictionary["$saferootprojectname$"] = replacementsDictionary["$safeprojectname$"];
+		}
+
+		public void RunFinished()
+		{
+		}
+
+		public void BeforeOpeningFile(ProjectItem projectItem)
+		{
+		}
+
+		public void ProjectFinishedGenerating(Project project)
+		{
+		}
+
+		public bool ShouldAddProjectItem(string filePath)
+		{
+			return true;
+		}
+
+		public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+		{
+		}
+	}
+}
