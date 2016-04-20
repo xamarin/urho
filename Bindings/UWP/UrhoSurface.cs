231b07521226d5d15a215edb94b34f65da1d8a0a
EgorBo <egorbo@gmail.com>
2016-04-19 23:43:59 -0400

    Add UrhoSurface for UWP. Copy pak files from Content to LocalFolder

--- Bindings/UWP/UrhoSurface.cs
+++ Bindings/UWP/UrhoSurface.cs
@@ -1,9 +1,75 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Windows.Storage;
+using Windows.System.Threading;
+using Windows.UI.Xaml.Controls;
 
 namespace Urho.UWP
 {
 	public class UrhoSurface : SwapChainPanel
 	{
-		//UWP native code will try to find SwapChainPanel in the Visual Tree
+		[DllImport(Consts.NativeImport, CallingConvention = CallingConvention.Cdecl)]
+		static extern void SDL_SetMainReady();
+
+		bool paused;
+		bool stop;
+
+		public TGame Run<TGame>(string customAssetsPak = null) where TGame : Urho.Application
+		{
+			stop = false;
+			paused = false;
+			SDL_SetMainReady();
+			ApplicationOptions options = new ApplicationOptions(assetsFolder: null);
+			if (!string.IsNullOrEmpty(customAssetsPak))
+			{
+				options.ResourcePackagesPaths = new[] { Path.GetFileName(customAssetsPak) };
+				CopyContentFileToLocalFolder(customAssetsPak);
+			}
+			CopyContentFileToLocalFolder("ms-appx:///Urho/CoreData.pak");
+			var app = (TGame)Activator.CreateInstance(typeof(TGame), options);
+			app.Run();
+			ThreadPool.RunAsync(_ =>
+			{
+				while (!stop)
+				{
+					if (!paused)
+						app.Engine.RunFrame();
+				}
+			});
+			return app;
+		}
+
+		public void Pause()
+		{
+			paused = true;
+		}
+
+		public void Resume()
+		{
+			paused = false;
+		}
+
+		public void Stop()
+		{
+			stop = true;
+		}
+
+		public static void CopyContentFileToLocalFolder(string msappx)
+		{
+			string file = Path.GetFileName(msappx);
+			try
+			{
+				//check if file exists
+				ApplicationData.Current.LocalFolder.GetFileAsync(file).GetAwaiter().GetResult();
+				return;
+			}
+			catch
+			{
+			}
+
+			var storageFile = StorageFile.GetFileFromApplicationUriAsync(new Uri(msappx)).GetAwaiter().GetResult();
+			storageFile.CopyAsync(ApplicationData.Current.LocalFolder).GetAwaiter().GetResult();
+		}
 	}
 }
--- Bindings/UWP/UwpUrhoInitializer.cs
+++ Bindings/UWP/UwpUrhoInitializer.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
+using System.Runtime.InteropServices;
 using Windows.Storage;
 
 namespace Urho.UWP
