1f20236db91b780c299d8c6fd0293e15afc1f0ab
EgorBo <egorbo@gmail.com>
2016-04-21 12:48:55 -0400

    Add warning for UWP that it works only in x86 yet

--- Bindings/UWP/UwpUrhoInitializer.cs
+++ Bindings/UWP/UwpUrhoInitializer.cs
@@ -10,6 +10,10 @@ namespace Urho.UWP
 		internal static void OnInited()
 		{
 			var folder = ApplicationData.Current.LocalFolder.Path;
+			if (IntPtr.Size == 8)
+			{
+				throw new NotSupportedException("x86_64 is not supported yet. Please use x86.");
+			}
 		}
 	}
 }
