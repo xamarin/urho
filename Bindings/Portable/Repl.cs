cc3cecfbe50e0f8d2d7f2fe735c8ee01f92fa8ce
EgorBo <egorbo@gmail.com>
2016-04-26 14:13:38 -0400

    Improve Repl stuff

--- Bindings/Portable/Repl.cs
+++ Bindings/Portable/Repl.cs
@@ -116,11 +116,24 @@ namespace Urho.Repl
 		protected override void OnUpdate(float timeStep)
 		{
 			if (MoveCamera)
-				SimpleMoveCamera3D(timeStep);
+			{
+				if (!Options.TouchEmulation)
+					SimpleMoveCamera3D(timeStep);
+				else
+					MoveCameraByTouches(timeStep);
+
+				float moveSpeed = 10.0f;
+
+				if (Input.GetKeyDown(Key.W)) CameraNode.Translate(Vector3.UnitZ * moveSpeed * timeStep);
+				if (Input.GetKeyDown(Key.S)) CameraNode.Translate(-Vector3.UnitZ * moveSpeed * timeStep);
+				if (Input.GetKeyDown(Key.A)) CameraNode.Translate(-Vector3.UnitX * moveSpeed * timeStep);
+				if (Input.GetKeyDown(Key.D)) CameraNode.Translate(Vector3.UnitX * moveSpeed * timeStep);
+			}
+			
 			base.OnUpdate(timeStep);
 		}
 
-		protected void SimpleMoveCamera3D(float timeStep, float moveSpeed = 10.0f)
+		protected void SimpleMoveCamera3D(float timeStep)
 		{
 			const float mouseSensitivity = .05f;
 
@@ -133,11 +146,35 @@ namespace Urho.Repl
 			Pitch = MathHelper.Clamp(Pitch, -90, 90);
 
 			CameraNode.Rotation = new Quaternion(Pitch, Yaw, 0);
+		}
 
-			if (Input.GetKeyDown(Key.W)) CameraNode.Translate(Vector3.UnitZ * moveSpeed * timeStep);
-			if (Input.GetKeyDown(Key.S)) CameraNode.Translate(-Vector3.UnitZ * moveSpeed * timeStep);
-			if (Input.GetKeyDown(Key.A)) CameraNode.Translate(-Vector3.UnitX * moveSpeed * timeStep);
-			if (Input.GetKeyDown(Key.D)) CameraNode.Translate(Vector3.UnitX * moveSpeed * timeStep);
+		protected void MoveCameraByTouches(float timeStep)
+		{
+			var input = Input;
+			for (uint i = 0, num = input.NumTouches; i < num; ++i)
+			{
+				TouchState state = input.GetTouch(i);
+				if (state.TouchedElement != null)
+					continue;
+
+				if (state.Delta.X != 0 || state.Delta.Y != 0)
+				{
+					var camera = CameraNode.GetComponent<Camera>();
+					if (camera == null)
+						return;
+
+					var graphics = Graphics;
+					Yaw += 2 * camera.Fov / graphics.Height * state.Delta.X;
+					Pitch += 2 * camera.Fov / graphics.Height * state.Delta.Y;
+					CameraNode.Rotation = new Quaternion(Pitch, Yaw, 0);
+				}
+				else
+				{
+					var cursor = UI.Cursor;
+					if (cursor != null && cursor.Visible)
+						cursor.Position = state.Position;
+				}
+			}
 		}
 	}
 
@@ -184,6 +221,22 @@ namespace Urho.Repl
 			var scale = barNode.Scale;
 			barNode.Position = new Vector3(pos.X, scale.Y / 2f, pos.Z);
 			textNode.Position = new Vector3(-0.5f, scale.Y + 0.5f, 0);
+		}
+	}
+
+	public static class Randoms
+	{
+		static readonly Random random = new Random();
+		
+		public static float Next(float min, float max)
+		{
+			return (float)((random.NextDouble() * (max - min)) + min);
+		}
+
+		public static float NextNormal(float min, float max, float normalPoint)
+		{
+			//TODO: optimize to O(1) and without linq;
+			return Enumerable.Range(1, 4).Select(_ => Next(min, max)).OrderBy(i => Math.Abs(i - normalPoint)).First();
 		}
 	}
 }
