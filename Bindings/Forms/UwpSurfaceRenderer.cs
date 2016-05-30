c10dc2fc3e1e33036f8ce97bdb5fec0a87d0c9ae
EgorBo <egorbo@gmail.com>
2016-05-30 18:30:56 +0300

    Xamarin.Forms UWP support. + update XF to 2.2.0.43

---
+++ Bindings/Forms/UwpSurfaceRenderer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Urho;
+using Urho.UWP;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRendererAttribute(typeof(Urho.Forms.UrhoSurface), typeof(Urho.Forms.UwpSurfaceRenderer2))]
+
+namespace Urho.Forms
+{
+
+	public class UwpSurfaceRenderer2 : ViewRenderer<Urho.Forms.UrhoSurface, Urho.UWP.UrhoSurface>
+	{
+		Urho.UWP.UrhoSurface urhoSurface = null;
+
+		protected override void OnElementChanged(ElementChangedEventArgs<UrhoSurface> e)
+		{
+			urhoSurface = new Urho.UWP.UrhoSurface();
+			e.NewElement.UrhoApplicationLauncher = UrhoLauncher;
+			SetNativeControl(urhoSurface);
+			base.OnElementChanged(e);
+		}
+
+		Task<Application> UrhoLauncher(Type type, ApplicationOptions opts)
+		{
+			var app = urhoSurface.Run(type, null);
+			return Task.FromResult(app);
+		}
+	}
+}
