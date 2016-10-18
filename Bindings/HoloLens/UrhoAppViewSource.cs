d274dd83a8b4a2270257acdffa40abf48911b657
EgorBo <egorbo@gmail.com>
2016-10-19 01:44:00 +0300

    Add UrhoAppViewSource (the easiest way to run apps on HoloLens).

---
+++ Bindings/HoloLens/UrhoAppViewSource.cs
@@ -0,0 +1,19 @@
+﻿using Windows.ApplicationModel.Core;
+using Urho.HoloLens;
+
+namespace Urho
+{
+	public class UrhoAppViewSource<T> : IFrameworkViewSource where T : HoloApplication
+	{
+		readonly string assets;
+
+		public UrhoAppViewSource() { }
+
+		public UrhoAppViewSource(string assets)
+		{
+			this.assets = assets;
+		}
+
+		public IFrameworkView CreateView() => UrhoAppView.Create<T>(assets);
+	}
+}
