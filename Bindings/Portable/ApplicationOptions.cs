8e8592c3d91a5a6c6c494c9bc69c62d887512ba2
Egor Bogatov <egorbo@gmail.com>
2016-11-29 23:09:55 +0100

    [Forms.iOS] Handle orientation changed

--- Bindings/Portable/ApplicationOptions.cs
+++ Bindings/Portable/ApplicationOptions.cs
@@ -50,7 +50,7 @@ namespace Urho
 		/// <summary>
 		/// iOS only
 		/// </summary>
-		public OrientationType Orientation { get; set; } = OrientationType.Portrait;
+		public OrientationType Orientation { get; set; } = OrientationType.LandscapeAndPortrait;
 #else
 		/// <summary>
 		/// iOS only
