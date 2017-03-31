ac6c3ba100b8294011ed9273864373533daa36a9
EgorBo <egorbo@gmail.com>
2017-04-01 02:36:56 +0300

    Texture2D: Add SetData accepting byte[] imagedata instead of a pointer

--- Bindings/Portable/Texture2D.cs
+++ Bindings/Portable/Texture2D.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Runtime.InteropServices;
-
 namespace Urho.Urho2D
 {
 	partial class Texture2D
@@ -8,6 +5,14 @@ namespace Urho.Urho2D
 		public bool SetSize(int width, int height, uint format, TextureUsage usage = TextureUsage.Static)
 		{
 			return SetSize(width, height, format, usage, 1, true);
+		}
+
+		public unsafe bool SetData(uint level, int x, int y, int width, int height, byte[] data)
+		{
+			fixed (byte* ptr = data)
+			{
+				return SetData(level, x, y, width, height, ptr);
+			}
 		}
 	}
 }
