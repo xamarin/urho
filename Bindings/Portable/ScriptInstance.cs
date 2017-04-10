bad7e10adc2f750fdb2e41d1669fb0fd1c993dcf
EgorBo <egorbo@gmail.com>
2017-04-11 01:49:07 +0300

    Add ScriptInstance stub

---
+++ Bindings/Portable/ScriptInstance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Urho.Portable
+{
+	[Preserve(AllMembers = true)]
+	public class ScriptInstance : Component
+	{
+		public ScriptInstance(Context context) : base(context)
+		{
+			Runtime.Validate(typeof(ScriptInstance));
+			handle = Component_Component((object)context == null ? IntPtr.Zero : context.Handle);
+			Runtime.RegisterObject(this);
+		}
+
+		public ScriptInstance(IntPtr handle) : base(handle) { }
+
+		protected ScriptInstance(UrhoObjectFlag emptyFlag) : base(emptyFlag) { }
+
+		public ScriptInstance() : this (Application.CurrentContext) { }
+	}
+}
