61db2f263dedb997d8a98e1e38dbb9b14a1a2697
EgorBo <egorbo@gmail.com>
2018-02-17 00:39:55 +0300

    forgot to add UrhoUWP.cpp/.h

---
+++ Urho3D/Urho3D_UWP/MonoUrho.UWP/UrhoUWP.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <wrl.h>
+#include <wrl/client.h>
+#include <dxgi1_4.h>
+#include <d3d11_3.h>
+#include <d2d1_3.h>
+#include <d2d1effects_2.h>
+#include <dwrite_3.h>
+#include <wincodec.h>
+#include <DirectXColors.h>
+#include <DirectXMath.h>
+#include <memory>
+#include <agile.h>
+#include <concrt.h>
+#include <collection.h>
+#include <windows.ui.xaml.media.dxinterop.h>
+#include <Urho3D/Engine/Engine.h>
+#include <Urho3D/Graphics/Camera.h>
+#include <Urho3D/Scene/Node.h>
+#include <SDL\SDL.h>
+
+using namespace D2D1;
+using namespace DirectX;
+using namespace Microsoft::WRL;
+using namespace Windows::Foundation;
+using namespace Windows::Graphics::Display;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Media;
+using namespace Platform;
+
